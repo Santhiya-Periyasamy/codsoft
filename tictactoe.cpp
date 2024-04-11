804ee867a7dbedae1b2bc129ead775c1348e969d
Santhiya-Periyasamy <165547873+Santhiya-Periyasamy@users.noreply.github.com>
2024-04-11 20:53:56 +0530

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -93,6 +93,7 @@ int main()
  {
     char player = 'X';
     int turn;
+    cout<<"---------WELCOME TO TIC-TAC-TOE ----------\n";
     cout<<"| 1| 2| 3|\n----------\n| 4| 5| 6|\n----------\n| 7| 8| 9|\n";
     for(turn = 0;turn<9;turn++)
     {
